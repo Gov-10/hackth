9247faf2070e84acbc345649b5c90ce96a2c4188
Govind S <govindsys1008@gmail.com>
2026-03-26 16:56:58 +0530

    Registered all models to admin dashboard

--- intelli-credit/backend/backapp/admin.py
+++ intelli-credit/backend/backapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import IntelliUser, Company, History
 # Register your models here.
+
+admin.site.register(IntelliUser)
+admin.site.register(Company)
+admin.site.register(History)
